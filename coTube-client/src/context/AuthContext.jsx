99498779607ccd0fc01ed70b809699bb611a76c7
vivek <vivan100123@gmail.com>
2026-07-08 19:14:22 +0530

    Add user field here to get the users email and userName

--- coTube-client/src/context/AuthContext.jsx
+++ coTube-client/src/context/AuthContext.jsx
@@ -3,28 +3,34 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(() => {
-        const stored = localStorage.getItem("user");
-        return stored ? JSON.parse(stored) : null;
-    });
+  const [user, setUser] = useState(() => {
+    const stored = localStorage.getItem("user");
+    return stored ? JSON.parse(stored) : null;
+  });
 
-    const loginUser = (response) => {
-        localStorage.setItem("token", response.token);
-        localStorage.setItem("user", JSON.stringify(response));
-        setUser(response);
+  const loginUser = (response) => {
+    localStorage.setItem("token", response.token);
+
+    const user = {
+      userName: response.userName,
+      email: response.email,
     };
 
-    const logout = () => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        setUser(null);
-    };
+    localStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, loginUser, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loginUser, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
