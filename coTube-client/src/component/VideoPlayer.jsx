d218e8fdf033c496df027f8e38da529df61b83e9
vivek <vivan100123@gmail.com>
2026-07-08 19:23:17 +0530

    add synchronized YouTube player with play, pause, and seek support.

--- coTube-client/src/component/VideoPlayer.jsx
+++ coTube-client/src/component/VideoPlayer.jsx
@@ -1,23 +1,84 @@
-export default function VideoPlayer() {
+import YouTube from "react-youtube";
+import { useRef , useEffect} from "react";
+
+export default function VideoPlayer({ videoId ,onPlay, onPause,remoteAction}) {
+
+  const playerRef = useRef(null);
+  const ignoreNextEvent = useRef(false);
+
+
+  useEffect(()=>{
+    if(!remoteAction || !playerRef.current) return;
+    switch (remoteAction.action){
+      case "PLAY":
+        ignoreNextEvent.current = true;
+        playerRef.current.seekTo(remoteAction.currentTime,true);
+        playerRef.current.playVideo();
+        break;
+
+      case "PAUSE":
+        ignoreNextEvent.current = true;
+        playerRef.current.seekTo(remoteAction.currentTime,true);
+        playerRef.current.pauseVideo();
+        break;
+
+      case "SEEK":
+        ignoreNextEvent.current = true;
+        playerRef.current.seekTo(remoteAction.currentTime,true);
+        break;
+      
+      default:
+        break;
+      
+    }
+  },[remoteAction]);
+
+  const onReady = (event) =>{
+    playerRef.current = event.target;
+    console.log("Player is ready.");
+  }
+
+  const onStateChange = (event) =>{
+
+    if(ignoreNextEvent.current){
+      ignoreNextEvent.current = false;
+      return;
+    }
+
+     switch(event.data){
+      case 1:
+        console.log("PLAY");
+        onPlay?.(playerRef.current);
+        break;
+      case 2:
+        console.log("PAUSE");
+        onPause?.(playerRef.current);
+        break;
+      default:
+        break;
+     }
+  }
+
+  if (!videoId) {
+    return (
+      <div className="border rounded p-6 text-center">
+        No video selected
+      </div>
+    );
+  }
 
   return (
-
-    <div className="border rounded-lg h-[500px] flex items-center justify-center">
-
-      <div className="text-center">
-
-        <h2 className="text-2xl font-bold">
-          Video Player
-        </h2>
-
-        <p className="text-gray-500 mt-2">
-          YouTube Player will be added here
-        </p>
-
-      </div>
-
-    </div>
-
+    <YouTube
+      videoId={videoId}
+      onReady={onReady}
+      onStateChange={onStateChange}
+      opts={{
+        width: "100%",
+        height: "500",
+        playerVars: {
+          autoplay:0,
+        },
+      }}
+    />
   );
-
 }
