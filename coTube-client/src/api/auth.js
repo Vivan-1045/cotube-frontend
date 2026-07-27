4655372d0fc4d591cddf4696237882dd6392678c
vivek <vivan100123@gmail.com>
2026-07-28 04:44:53 +0530

    added the api endpoints for verification of email and resend the link.

--- coTube-client/src/api/auth.js
+++ coTube-client/src/api/auth.js
@@ -8,3 +8,11 @@ export const loginUser = (data) =>{
     return api.post('/auth/login',data);
 }
 
+export const verifyEmail = (token) => {
+    return api.get(`/auth/verify-email?token=${encodeURIComponent(token)}`);
+};
+
+export const resendVerificationEmail = (email) =>{
+    return api.post('/auth/resend-verification',{email});
+}
+
