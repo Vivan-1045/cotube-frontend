a2cf3f93764d403372b7fd459381f95834bc9c75
vivek <vivan100123@gmail.com>
2026-08-12 02:07:29 +0530

    import the app.css in main so that all content releted to css would be used globally.

--- coTube-client/src/main.jsx
+++ coTube-client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import "./App.css";
 
 import { AuthProvider } from "./context/AuthContext";
 
